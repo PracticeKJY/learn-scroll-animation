87eda1f13e65d284f9e43dd07055b4346d44d6e1
PracticeKJY <poiijtl1004@naver.com>
2023-10-29 17:14:03 +0900

    feat. 스크롤애니메이션 예제 구현

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+  },
+}
 
 module.exports = nextConfig
